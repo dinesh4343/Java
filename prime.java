42a4a3a35cb38a55da0f6e65036a19ff2a49e802
dinesh4343 <dineshpandian008@gmail.com>
2025-06-25 14:00:12 +0530

    Add initial implementation of prime number checker

---
+++ prime.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class prime {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int c = 0;
+
+        for (int i = 2; i <= n; i++) {
+
+        }
+    }
+}
